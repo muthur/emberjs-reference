b2bde80f7402cddf57c170bbcb667ce5c8e3e7ec
muthu <muthunagaswamy@gmail.com>
2018-03-20 15:55:43 -0700

    Added additional user acceptance test to check the first item rendered

--- tests/acceptance/users-index-test.js
+++ tests/acceptance/users-index-test.js
@@ -8,11 +8,13 @@ module('Acceptance | users index', (hooks) => {
   setupMirage(hooks);
 
   test('visiting /users should display user information', async (assert) => {
-    server.createList("user", 5);
+    let users = server.createList("user", 5);
     await visit('/users');
 
     assert.equal(currentURL(), '/users');
     assert.equal(find('.heading').textContent, 'User List'); //EmberJs for testing is moving away from Jquery dependency
     assert.equal(findAll('.row-item').length,5);
+    assert.equal(findAll('.test-user-firstName')[0].textContent,users[0].firstName);
+    
   });
 });
