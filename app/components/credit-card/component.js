86c7c717806a407220566cdcdb4c862dfe1b6b34
muthur <muthunagaswamy@gmail.com>
2018-08-07 09:55:07 -0700

    fixed dropdown for displaying countries

--- app/components/credit-card/component.js
+++ app/components/credit-card/component.js
@@ -1,4 +1,12 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+    countries: null,
+    init () {
+        this._super(...arguments);
+        this.countries = [
+          { abbrev: 'US' },
+          { abbrev: 'AUS' },
+        ];
+      }
 });
--- app/routes/payment-detail.js
+++ app/routes/payment-detail.js
@@ -1,10 +1,4 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-  setupController: function (controller) {
-    controller.set('countries', [
-      { abbrev: 'US' },
-      { abbrev: 'AUS' },
-    ]);
-  }
 });
