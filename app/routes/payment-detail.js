6db580c0447b7ddac3fa790311c6bb0fb69531c7
Muthunagaswamy Ramasubramanian <mramasubramanian@ingenio.com>
2018-08-07 15:24:31 -0700

    Modified the code to display the server side error (generally coming from ember data object) within the ember paper input

--- app/routes/payment-detail.js
+++ app/routes/payment-detail.js
@@ -1,4 +1,10 @@
-import Route from '@ember/routing/route';
+import Route from "@ember/routing/route";
 
 export default Route.extend({
+  setupController: function(controller) {
+    controller.set("errors", {
+      cardNumber: ["Card number is invalid"]
+    });
+    controller.set("cardNumber",7867777867867876);
+  }
 });
