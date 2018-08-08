5e85a4f549e2e01cea7f35bcc2348d370cc7d659
muthur <muthunagaswamy@gmail.com>
2018-08-07 23:13:10 -0700

    Added custom validation to validate credit card

--- app/routes/payment-detail.js
+++ app/routes/payment-detail.js
@@ -2,9 +2,9 @@ import Route from "@ember/routing/route";
 
 export default Route.extend({
   setupController: function(controller) {
-    controller.set("errors", {
+    /*controller.set("errors", {
       cardNumber: ["Card number is invalid"]
-    });
+    });*/
     controller.set("cardNumber",7867777867867876);
   }
 });
