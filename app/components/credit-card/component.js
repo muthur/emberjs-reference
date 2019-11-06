a6393104a255f4dd795c728f984275afb0ff3bf2
Muthu R <muthunagaswamy@gmail.com>
2019-11-05 22:38:05 -0800

    Added onValidityChange handlers for individual form input controls
    - Based on this ember paper form code it looks like individual form input can respond to validity change
    https://github.com/miguelcobain/ember-paper/blob/19d103c3d82b845a15c2c90dc63daf421da29a23/addon/templates/components/paper-form.hbs

--- app/components/credit-card/component.js
+++ app/components/credit-card/component.js
@@ -6,25 +6,31 @@ export default Component.extend({
   init() {
     this._super(...arguments);
     this.countries = [{ abbrev: "US" }, { abbrev: "AUS" }];
-    this.cardNumberValidations = [
-      {
-        message: "Invalid card number",
-        validate: cardNumber => {
-          return (
-            creditCardValidator.validateLuhn(cardNumber) &&
-            creditCardValidator.isValidNumber(cardNumber.toString())
-          );
-        }
+    this.set('cardNumberValidation', [{
+      message: "Invalid card number",
+      validate: cardNumber => {
+        return (
+          creditCardValidator.validateLuhn(cardNumber) &&
+          creditCardValidator.isValidNumber(cardNumber.toString())
+        );
       }
-    ];
-    this.cvvValidations = [
-      {
-        message: "Invalid CVV number provided",
-        validate: cvvNumber => {
-          return cvvNumber > 200;
-        }
+    }]);
+    this.set('cvvValidations', [{
+      message: "Invalid CVV number provided",
+      validate: cvvNumber => {
+        // eslint-disable-next-line no-console
+        console.log('cvv : ' + cvvNumber);
+        return cvvNumber > 200;
       }
-    ];
+    }]);
+    this.set('expirationDateValidations', [{
+      message: "Invalid Expiration Date provided",
+      validate: expirationDate => {
+        // eslint-disable-next-line no-console
+        console.log('expirationDate : ' + expirationDate);
+        return expirationDate === '092020';
+      }
+    }]);
   },
 
   actions: {
@@ -41,7 +47,11 @@ export default Component.extend({
     },
     onCreditCardNumberChange(obj) {
       console.log(obj);
-    }
+    },
+    onCreditCardFieldChange(id, value) {
+      this.set(id, value);
+      console.log('onCreditCardFieldChange: id=' + id + ', value=' + value);
+    },
   }
 
   //https://github.com/arenoir/ember-credit-cards/tree/master/addon/utils CC helper utils
